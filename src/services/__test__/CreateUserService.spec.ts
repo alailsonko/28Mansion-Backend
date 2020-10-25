116d7e9312aa7df3d010bd3abe91cdbcdec778ff
alailsonko <aalailson3@gmail.com>
2020-10-25 20:53:43 -0300

    test: throws error if email is not provided

--- src/services/__test__/CreateUserService.spec.ts
+++ src/services/__test__/CreateUserService.spec.ts
@@ -15,4 +15,18 @@ describe('CreateUserService', () => {
 
     expect(response.statusCode).toBe(400)
   })
+  test('should return 400 if email is missing', () => {
+    const sut = new CreateUserService()
+
+    const response = sut.execute({
+      body: {
+        username: 'valid_username',
+        email: '',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    })
+
+    expect(response.statusCode).toBe(400)
+  })
 })

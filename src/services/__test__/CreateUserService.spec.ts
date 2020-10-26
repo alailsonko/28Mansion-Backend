2e9edc3afb5df54f026b55736a471a1eb5c0650f
alailsonko <aalailson3@gmail.com>
2020-10-25 23:44:39 -0300

    test:  verify if user already exist , hash password, and save user and return the user

--- src/services/__test__/CreateUserService.spec.ts
+++ src/services/__test__/CreateUserService.spec.ts
@@ -1,10 +1,37 @@
 import CreateUserService from '../CreateUserService'
+import { getConnection } from 'typeorm'
+import User from '../../database/entities/User'
+
+/* eslint-disable jest/expect-expect */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import connection from '../../database/index'
+
+let id = null
+console.log(id)
+beforeAll(async () => {
+  await connection
+})
+
+afterAll(async () => {
+  await getConnection().close()
+})
+
+afterAll(async () => {
+  console.log(id)
+  await getConnection()
+    .createQueryBuilder()
+    .delete()
+    .from(User)
+    .where('id = :id', { id: id })
+    .execute()
+})
 
 describe('CreateUserService', () => {
-  test('should return 400 if username is missing', () => {
+  test('should return 400 if username is missing', async () => {
     const sut = new CreateUserService()
 
-    const response = sut.execute({
+    const response = await sut.execute({
       body: {
         username: '',
         email: 'valid_email@mail.com',
@@ -15,10 +42,10 @@ describe('CreateUserService', () => {
 
     expect(response.statusCode).toBe(400)
   })
-  test('should return 400 if email is missing', () => {
+  test('should return 400 if email is missing', async () => {
     const sut = new CreateUserService()
 
-    const response = sut.execute({
+    const response = await sut.execute({
       body: {
         username: 'valid_username',
         email: '',
@@ -29,10 +56,10 @@ describe('CreateUserService', () => {
 
     expect(response.statusCode).toBe(400)
   })
-  test('should return 400 if password is missing', () => {
+  test('should return 400 if password is missing', async () => {
     const sut = new CreateUserService()
 
-    const response = sut.execute({
+    const response = await sut.execute({
       body: {
         username: 'valid_username',
         email: 'valid_email@mail.com',
@@ -43,10 +70,10 @@ describe('CreateUserService', () => {
 
     expect(response.statusCode).toBe(400)
   })
-  test('should return 400 if passwordConfirmation is missing', () => {
+  test('should return 400 if passwordConfirmation is missing', async () => {
     const sut = new CreateUserService()
 
-    const response = sut.execute({
+    const response = await sut.execute({
       body: {
         username: 'valid_username',
         email: 'valid_email@mail.com',
@@ -57,10 +84,10 @@ describe('CreateUserService', () => {
 
     expect(response.statusCode).toBe(400)
   })
-  test('should return 400 if password does not match is missing', () => {
+  test('should return 400 if password does not match is missing', async () => {
     const sut = new CreateUserService()
 
-    const response = sut.execute({
+    const response = await sut.execute({
       body: {
         username: 'valid_username',
         email: 'valid_email@mail.com',
@@ -71,10 +98,10 @@ describe('CreateUserService', () => {
 
     expect(response.statusCode).toBe(400)
   })
-  test('should return 400 if email is invaid', () => {
+  test('should return 400 if email is invaid', async () => {
     const sut = new CreateUserService()
 
-    const response = sut.execute({
+    const response = await sut.execute({
       body: {
         username: 'valid_username',
         email: 'invalid_email @mail.com',
@@ -84,5 +111,22 @@ describe('CreateUserService', () => {
     })
 
     expect(response.statusCode).toBe(400)
+  })
+  test('should return 200 should create user is fit requeriments', async () => {
+    const sut = new CreateUserService()
+
+    const response = await sut.execute({
+      body: {
+        username: 'valid_username',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    })
+
+    id = response.statusMessage.user.id
+    console.log(response.statusMessage)
+
+    expect(response.statusCode).toBe(200)
   })
 })

6556d5cb254e8529d28d1aca5521dd47d32f0223
alailsonko <aalailson3@gmail.com>
2020-10-26 00:32:58 -0300

    test: implements integration databse, routes

--- src/routes/__test__/signup.test.ts
+++ src/routes/__test__/signup.test.ts
@@ -21,7 +21,17 @@ afterAll(async () => {
 
 describe('Test route /signup', () => {
   test('should return successful case route is working', async (done) => {
-    const response = await request.get('/signup')
+    const data = {
+      username: 'valid_username',
+      email: 'valid_mail@mail.com',
+      password: 'valid_password',
+      passwordConfirmation: 'valid_password'
+    }
+
+    const response = await request.post('/signup').send(data)
+      .set('Accept', 'application/json')
+
+    console.log(response)
     expect(response.status).toBe(200)
     done()
   })

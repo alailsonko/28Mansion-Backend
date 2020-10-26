9ca890a07d9ef415dbf7f1a4c2cc1bdcbc75de26
alailsonko <aalailson3@gmail.com>
2020-10-26 04:24:34 -0300

    fix: tries prod version

--- ormconfig.js
+++ ormconfig.js
@@ -11,6 +11,9 @@ module.exports = {
   password: `${process.env.POSTGRES_PASSWORD}`,
   username: `${process.env.POSTGRES_USERNAME}`,
   database: `${process.env.POSTGRES_DATABASE}`,
+  extra: {
+    ssl: true
+  },
   entities: entities,
   migrations: migrations,
   cli: {

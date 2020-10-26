3093b657cb04811b8ef124f387af8b5b676bc29e
alailsonko <aalailson3@gmail.com>
2020-10-26 04:12:31 -0300

    fix: tries prod version

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 const entities = process.env.NODE_ENV === 'development' ? ['./dist/database/entities/*.js'] : ['./src/database/entities/*.{js,ts}']
 const migrations = process.env.NODE_ENV === 'development' ? ['./dist/database/migrations/*.js'] : ['./src/database/migrations/*.{js,ts}']
 const migrationsDir = process.env.NODE_ENV === 'development' ? './dist/database/migrations' : './src/database/migrations'
 const entitiesDir = process.env.NODE_ENV === 'development' ? './dist/database/entities' : './src/database/entities'
+const fs = require('fs')
 
 module.exports = {
   url: process.env.NODE_ENV === 'development' ? process.env.POSTGRES_URI : null,
@@ -11,7 +13,8 @@ module.exports = {
   password: `${process.env.POSTGRES_PASSWORD}`,
   username: `${process.env.POSTGRES_USERNAME}`,
   database: `${process.env.POSTGRES_DATABASE}`,
-  entities: entities,
+  // eslint-disable-next-line no-path-concat
+  entities: [__dirname + '/../**/**.entities{.ts,.js}'],
   migrations: migrations,
   cli: {
     migrationsDir: migrationsDir,

d0fc0850bf188d68f541745b610e3d7195215e6d
alailsonko <aalailson3@gmail.com>
2020-10-26 04:19:39 -0300

    fix: tries prod version

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-unused-vars */
-const entities = process.env.NODE_ENV === 'development' ? ['./dist/database/entities/*.js'] : ['./src/database/entities/*.{js,ts}']
-const migrations = process.env.NODE_ENV === 'development' ? ['./dist/database/migrations/*.js'] : ['./src/database/migrations/*.{js,ts}']
+const entities = process.env.NODE_ENV === 'development' ? ['./dist/database/entities/*.{js,ts}'] : ['./src/database/entities/*.{js,ts}']
+const migrations = process.env.NODE_ENV === 'development' ? ['./dist/database/migrations/*.{js,ts}'] : ['./src/database/migrations/*.{js,ts}']
 const migrationsDir = process.env.NODE_ENV === 'development' ? './dist/database/migrations' : './src/database/migrations'
 const entitiesDir = process.env.NODE_ENV === 'development' ? './dist/database/entities' : './src/database/entities'
-const fs = require('fs')
 
 module.exports = {
   url: process.env.NODE_ENV === 'development' ? process.env.POSTGRES_URI : null,
@@ -13,8 +11,7 @@ module.exports = {
   password: `${process.env.POSTGRES_PASSWORD}`,
   username: `${process.env.POSTGRES_USERNAME}`,
   database: `${process.env.POSTGRES_DATABASE}`,
-  // eslint-disable-next-line no-path-concat
-  entities: './{src,dist}/**/entities/*.{ts,js}',
+  entities: entities,
   migrations: migrations,
   cli: {
     migrationsDir: migrationsDir,

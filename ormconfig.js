24d3b5b74086cc8974ad6d194f4f8c98ff060638
alailsonko <aalailson3@gmail.com>
2020-10-26 05:04:53 -0300

    fix: tries prod version

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,4 @@
-/* eslint-disable no-unused-vars */
-const path = require('path')
-const entities = process.env.NODE_ENV === 'development' ? ['./dist/database/entities/*.{js,ts}'] : ['./src/database/entities/*.{js,ts}']
+const entities = process.env.NODE_ENV === 'development' ? 'dist/database/entities/*.{js,ts}' : 'src/database/entities/*.{js,ts}'
 const migrations = process.env.NODE_ENV === 'development' ? ['./dist/database/migrations/*.{js,ts}'] : ['./src/database/migrations/*.{js,ts}']
 const migrationsDir = process.env.NODE_ENV === 'development' ? './dist/database/migrations' : './src/database/migrations'
 const entitiesDir = process.env.NODE_ENV === 'development' ? './dist/database/entities' : './src/database/entities'
@@ -13,10 +11,7 @@ module.exports = {
   password: `${process.env.POSTGRES_PASSWORD}`,
   username: `${process.env.POSTGRES_USERNAME}`,
   database: `${process.env.POSTGRES_DATABASE}`,
-  entities: [
-    // assuming _dirname is your project root
-    path.join(__dirname, '/**/database/entities/{.ts,.js}')
-  ],
+  entities: entities,
   migrations: migrations,
   cli: {
     migrationsDir: migrationsDir,

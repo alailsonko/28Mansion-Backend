cfc917e5c02e074953b936c01c9c562ae96066e8
alailsonko <aalailson3@gmail.com>
2020-10-27 15:42:56 -0300

    feat: implements create user

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { hash } from 'bcryptjs'
+import { getRepository } from 'typeorm'
+import User from '../entities/User'
+
+interface StoredData {
+  username: string
+  email: string
+  password: string
+}
+
+interface HttpResponse {
+  statusCode: number
+  statusMessage: any
+}
+
+async function CreateUserService (data: StoredData): Promise<HttpResponse> {
+  const usersRepository = getRepository(User)
+
+  const checkUserExists = await usersRepository.findOne({
+    where: { email: data.email }
+  })
+
+  if (checkUserExists) {
+    return { statusCode: 400, statusMessage: 'User already exists' }
+  }
+  const passwordHashed = await hash(data.password, 8)
+  const targetData = Object.assign(data, { password: passwordHashed })
+  console.log(targetData)
+  await usersRepository.save(targetData)
+  console.log(targetData)
+
+  return { statusCode: 200, statusMessage: 'user created successfully' }
+}
+
+export default CreateUserService

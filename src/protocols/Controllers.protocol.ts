c298faafbd3833b772d609eb2e76a4128a83fb20
alailsonko <aalailson3@gmail.com>
2020-10-28 19:31:51 -0300

    feat: create postcontroller interface

--- src/protocols/Controllers.protocol.ts
+++ src/protocols/Controllers.protocol.ts
@@ -4,3 +4,11 @@ export interface IUserController {
   SignUp: (req: Request, res: Response) => Promise<Response>
   SignIn: (req: Request, res: Response) => Promise<Response>
 }
+
+export interface IPostController {
+  GetAllPosts: (req: Request, res: Response) => Promise<Response>
+  GetPostById: (req: Request, res: Response) => Promise<Response>
+  CreateNewPost: (req: Request, res: Response) => Promise<Response>
+  UpdatePost: (req: Request, res: Response) => Promise<Response>
+  DeletePost: (req: Request, res: Response) => Promise<Response>
+}

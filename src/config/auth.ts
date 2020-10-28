e553cd67212b72bbbeb3c2883dd4126ddacc79ca
alailsonko <aalailson3@gmail.com>
2020-10-28 00:16:59 -0300

    fix: expiresin

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
     secret: `${process.env.JSONWEBTOKEN_SECRET}`,
-    expiresIn: `${process.env.JSONWEBETOKEN_EXPIRESIN}`
+    expiresIn: `${process.env.JSONWEBTOKEN_EXPIRESIN}`
   }
 }

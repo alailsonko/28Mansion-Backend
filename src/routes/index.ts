a7933dac8ddeaab120044ca25d76c1dc2c5a464f
alailsonko <aalailson3@gmail.com>
2020-10-28 19:33:36 -0300

    feat: add posts routes on index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import usersRouter from './users.routes'
+import postsRouter from './posts.routes'
 
 const routes = Router()
 
 routes.use('', usersRouter)
+routes.use('', postsRouter)
 
 export default routes

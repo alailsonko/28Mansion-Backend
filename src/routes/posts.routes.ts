99064c86633a3f87ec100cddb42a26518dcab204
alailsonko <aalailson3@gmail.com>
2020-10-29 04:55:54 -0300

    feat: implements authentication to postsroutes

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express'
 import PostController from '../controllers/PostController'
+import ensureAuthentication from '../middlewares/ensureAuthentication'
 
 const makePostController = new PostController()
 
 const postsRouter = Router()
+
+postsRouter.use(ensureAuthentication)
 
 // get all posts
 postsRouter.get('/posts', makePostController.GetAllPosts)
 // get post by id
 postsRouter.get('/posts/:id', makePostController.GetPostById)
 // create a new post
-postsRouter.post('/posts/:id', makePostController.CreateNewPost)
+postsRouter.post('/posts', makePostController.CreateNewPost)
 // change the post
 postsRouter.put('/posts/:id', makePostController.UpdatePost)
 // delete post

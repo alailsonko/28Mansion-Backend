511bd3cd624f75bde39ff996b3f4a9fc68b95012
alailsonko <aalailson3@gmail.com>
2021-03-18 12:11:03 -0300

    chore: edit

--- ormconfig.js
+++ ormconfig.js
@@ -11,12 +11,11 @@ const migrationsLocalProd = ['./dist/src/migrations/*.js']
 
 module.exports = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
-  // host: process.env.POSTGRES_HOST,
-  // port: process.env.POSTGRES_PORT,
-  // username: process.env.POSTGRES_USERNAME,
-  // password: process.env.POSTGRES_PASSWORD,
-  // database: process.env.POSTGRES_DATABASE,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
 
   // // dev variables
 
